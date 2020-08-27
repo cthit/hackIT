ab96b01cdb9d47bf2ba820eb727a2d5eea1f0467
Joakim <joakimanderlind@gmail.com>
2020-08-27 13:06:12 +0200

    Add oauth handling

--- hackIT/src/main.rs
+++ hackIT/src/main.rs
@@ -5,7 +5,20 @@
 
 use serde::Serialize;
 use rocket::State;
+
+use rocket::http::{Cookie, Cookies, RawStr};
+use rocket::response::{Flash, Redirect};
 use rocket_contrib::templates::Template;
+
+
+use oauth2::basic::BasicClient;
+
+use oauth2::reqwest::http_client;
+use oauth2::{
+    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, RedirectUrl, Scope,
+    TokenResponse, TokenUrl,
+};
+use std::env;
 
 pub mod db;
 pub mod challenge;
@@ -45,26 +58,77 @@ fn challenges(chs : State<ConstState>) -> Template {
 }
 
 #[get("/")]
-fn index() -> Template {
+fn index(chs : State<ConstState>, mut cookies: Cookies) -> Template {
 
     #[derive(Serialize)]
-    struct Context {
+    struct Context<'a>{
 	name : &'static str,
+    auth : String,
+    auth_url : &'a str,
     }
-    let ctx = Context{ name : "Yoda" };
+
+    let tkn = match cookies.get_private("gamma_access_token") {
+        Some(c) => c.value().to_string(),
+        _       => "You are not logged in...".to_string(),
+    };
+
+    let ctx = Context{ name : "Yoda", auth : tkn, auth_url : &chs.auth_url};
     
     Template::render("index",&ctx)
 }
 
+#[get("/auth/gamma?<code>&<state>")]
+fn gamma_auth(chs : State<ConstState>,mut cookies: Cookies, code : &RawStr, state : &RawStr) -> Redirect {
+    let token = chs.oauth
+        .exchange_code(AuthorizationCode::new(code.to_string()))
+        .request(http_client).unwrap();
+
+    cookies.add_private(Cookie::new("gamma_access_token",token.access_token().secret().to_string()));
+    Redirect::to("/")
+
+}
+
 struct ConstState{
     challenges : Challenges,
+    oauth      : oauth2::basic::BasicClient,
+    auth_url   : String,
 }
 
 fn main() {
+
+    
+    let gamma_client_id = ClientId::new(
+        env::var("GAMMA_CLIENT_ID").expect("Missing the GAMMA_CLIENT_ID environment variable."),
+    );
+    let gamma_client_secret = ClientSecret::new(
+        env::var("GAMMA_CLIENT_SECRET")
+            .expect("Missing the GAMMA_CLIENT_SECRET environment variable."),
+    );
+    let auth_url = AuthUrl::new("http://localhost:8081/api/oauth/authorize".to_string())
+        .expect("Invalid authorization endpoint URL");
+    let token_url = TokenUrl::new("http://gamma-backend:8081/api/oauth/token".to_string())
+        .expect("Invalid token endpoint URL");
+
+    let client = BasicClient::new(
+        gamma_client_id,
+        Some(gamma_client_secret),
+        auth_url,
+        Some(token_url),
+    )
+
+    .set_redirect_url(
+        RedirectUrl::new("http://localhost:8000/auth/gamma".to_string()).expect("Invalid redirect URL"),
+    );
+
+    let (authorize_url, csrf_state) = client
+        .authorize_url(CsrfToken::new_random)
+        .url();
+    
+
     rocket::ignite()
 	.attach(Template::fairing())
 	.attach(UserRecordsConn::fairing())
-	.manage(ConstState{ challenges : load_challenges("test_challenges") })
-	.mount("/", routes![index,records,challenges]).launch();
+	.manage(ConstState{ challenges : load_challenges("test_challenges"), oauth : client, auth_url : authorize_url.to_string()})
+	.mount("/", routes![index,records,challenges,gamma_auth]).launch();
 }
 
